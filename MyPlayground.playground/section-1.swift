22e080c7e263ff3ef95df9c7fa63e46b164fd211
iceesj <icsj@icloud.com>
2014-06-08 00:10:30 +0800

    201406080010

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,38 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var languageName1:String = "Swift" //inferred as String
+var version1:Double = 1.0 //inferred as double
+var introduced1:Int = 2014 //inferred as int
+var isAwesome1:Bool = true// inferred as bool
+
+let languageName = "Swift"
+var version = 1.0
+let introduced = 2014
+let isAwesome = true
+
+let 你猜 = "nicai"
+let 😄 = "smile"
+println(你猜+😄)
+
+let someString = "I appear to be a string"
+let components = "~/Documents/Swift".pathComponents
+
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+shoppingList[1] = "bottle of water"
+println(shoppingList)
+
+
+var dict = ["dict1":"nicai","dict2":"nicai2",]
+var nicai = "nicainicai"
+var zaijian = "zaijian"
+
+
+//for num in array{
+//    println(num)
+//}
+
+
