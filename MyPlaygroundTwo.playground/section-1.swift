f1d3a92b948c52134e6c36e02a3c8546c9f3b327
iceesj <icsj@icloud.com>
2014-06-08 02:01:02 +0800

    2014,0608,02:00

---
+++ MyPlaygroundTwo.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+//Room 102
+
+var str = "WWDC"
+
+let date = NSDate()
+
+let font = NSFont(name: "Avenir Next Condensed Heavy",size: 100.0)
+let attributedString = NSAttributedString(string:str,attributes: [NSFontAttributeName:font])
+
+
